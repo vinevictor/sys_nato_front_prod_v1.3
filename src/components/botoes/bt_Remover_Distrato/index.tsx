eba33697a9817e9ebf4617b502e8773d1d10aaad
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-06-10 16:51:25 -0300

    update

--- src/components/botoes/bt_Remover_Distrato/index.tsx
+++ src/components/botoes/bt_Remover_Distrato/index.tsx
@@ -1,61 +1,28 @@
 "use client";
-import { useSession } from "@/hook/useSession";
 import { Button, useToast } from "@chakra-ui/react";
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { BeatLoader } from "react-spinners";
-import { AuthUser } from "@/types/session";
 
 interface BtRemoverDistratoProps {
   id: number | null;
-  user: any;
 }
 export default function BtRemoverDistrato({
   id,
-  user,
 }: BtRemoverDistratoProps) {
   const [Loading, setLoading] = useState<boolean>(false);
   const toast = useToast();
-  const User = user;
   const route = useRouter();
 
   const handleClick = async () => {
     setLoading(true);
     try {
-      const Get = await fetch(`/api/solicitacao/get/${id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      const req = await Get.json();
-
-      const res = await fetch(`/api/solicitacao/update/${id}`, {
+      const res = await fetch(`/api/solicitacao/novo-acordo/${id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          distrato: false,
-          distrato_id: null,
-          distrato_dt: null,
-          logDelete: `${req.logDelete}\nO usuário: ${User?.nome}, id: ${
-            User?.id
-          } Firmou novo acordo para esse registro em: ${new Date().toLocaleDateString(
-            "pt-BR"
-          )} as ${new Date().toLocaleTimeString("pt-BR")}`,
-          obs: req.obs
-            ? `${
-                req.obs
-              }\nNovo Acordo firmado em: ${new Date().toLocaleDateString(
-                "pt-BR"
-              )} as ${new Date().toLocaleTimeString("pt-BR")}`
-            : `Novo Acordo firmado em: ${new Date().toLocaleDateString(
-                "pt-BR"
-              )} as ${new Date().toLocaleTimeString("pt-BR")}`,
-        }),
       });
 
       if (res.ok) {
@@ -85,7 +52,12 @@ export default function BtRemoverDistrato({
   return (
     <Button
       size={"sm"}
-      colorScheme="cyan"
+      bg="gray.600"
+      color="white"
+      _hover={{ bg: "gray.700" }}
+      _active={{ bg: "gray.700" }}
+      _focus={{ bg: "gray.700" }}
+      px={8}
       variant="solid"
       onClick={handleClick}
       isLoading={Loading}
