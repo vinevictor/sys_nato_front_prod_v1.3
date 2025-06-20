efd873255539a2596dc840583e446599fc97f0e3
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-06-20 17:32:06 -0300

    fix: corrigir erros de dynamic server 🔧

--- src/hook/useAlertContext.tsx
+++ src/hook/useAlertContext.tsx
@@ -1,6 +1,8 @@
 import { AlertContext } from "@/context/AlertContext";
 import { useContext } from "react";
 
+export const dynamic = "force-dynamic";
+
 export default function useAlertContext() {
   const context = useContext(AlertContext);
 
