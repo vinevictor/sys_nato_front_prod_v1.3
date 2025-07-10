5bff61973ab925e84676fc9cac6ef4dd0ce588d3
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-07-10 16:35:14 -0300

    refactor
    [#124] select do CORRETOR não esta caregando em Cadastro Nova Solicitação, NÃO
    
    https://jira.arinterface.com.br/work_packages/124

--- src/app/api/construtora/get/[id]/route.ts
+++ src/app/api/construtora/get/[id]/route.ts
@@ -1,6 +1,6 @@
-// import { GetSessionServer } from '@/lib/auth_confg';
-// import crypto from 'crypto';
-// import { NextResponse } from 'next/server';
+import { GetSessionServer } from "@/lib/auth_confg";
+import crypto from "crypto";
+import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
@@ -8,49 +8,52 @@ export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  // try {
-  //   const { id } = params;
-  //   if (!id) {
-  //     return NextResponse.json({ message: "ID da Construtora não fornecido" }, { status: 400 });
-  //   }
-  //   const session = await GetSessionServer();
-  //   if (!session) {
-  //     return new NextResponse("Unauthorized", { status: 401 });
-  //   }
-  //   const req = await fetch(
-  //     `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/construtora/${id}`,
-  //     {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${session?.token}`,
-  //       },
-  //     }
-  //   );
-  //   if (!req.ok) {
-  //     return new NextResponse("ERRO", { status: 401 });
-  //   }
-  //   const data = await req.json();
-  //   const bodyString = JSON.stringify(data);
-  //   const etag = crypto.createHash('md5').update(bodyString).digest('hex');
-  //   // Verificar se o cliente enviou If-None-Match com esse ETag
-  //   const ifNoneMatch = request.headers.get('If-None-Match');
-  //   if (ifNoneMatch === etag) {
-  //     // Conteúdo não mudou
-  //     return new NextResponse(null, { status: 304 });
-  //   }
-  //   // Retornar dados com ETag no header
-  //   return NextResponse.json(
-  //     { error: false, message: "Sucesso", data: data },
-  //     {
-  //       status: 200,
-  //       headers: {
-  //         'ETag': etag,
-  //         'Cache-Control': 'private, max-age=0, must-revalidate',
-  //       }
-  //     }
-  //   );
-  // } catch (error: any) {
-  //   return NextResponse.json({ error: error }, { status: 500 });
-  // }
+  try {
+    const { id } = params;
+    if (!id) {
+      return NextResponse.json(
+        { message: "ID da Construtora não fornecido" },
+        { status: 400 }
+      );
+    }
+    const session = await GetSessionServer();
+    if (!session) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const req = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/construtora/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${session?.token}`,
+        },
+      }
+    );
+    if (!req.ok) {
+      return new NextResponse("ERRO", { status: 401 });
+    }
+    const data = await req.json();
+    const bodyString = JSON.stringify(data);
+    const etag = crypto.createHash("md5").update(bodyString).digest("hex");
+    // Verificar se o cliente enviou If-None-Match com esse ETag
+    const ifNoneMatch = request.headers.get("If-None-Match");
+    if (ifNoneMatch === etag) {
+      // Conteúdo não mudou
+      return new NextResponse(null, { status: 304 });
+    }
+    // Retornar dados com ETag no header
+    return NextResponse.json(
+      { error: false, message: "Sucesso", data: data },
+      {
+        status: 200,
+        headers: {
+          ETag: etag,
+          "Cache-Control": "private, max-age=0, must-revalidate",
+        },
+      }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
 }
