4b92f5c940729fb6c423a1149f423c4c9a75cca9
V. Matheus Almeida <matheus_famas@hotmail.com>
2025-07-28 15:14:36 -0300

    direto user

--- src/components/direto/user/index.tsx
+++ src/components/direto/user/index.tsx
@@ -1,7 +1,9 @@
 import { BugReport } from "@/components/bug";
 import { Flex, FormLabel, Text } from "@chakra-ui/react";
-import { mask } from "remask";
+
 import { BtnListNow } from "../imputs/BtnListNow";
+import { FinanceiraLinks } from "../data";
+import { mask } from "remask";
 
 interface UserCompomentInfoProps {
   session: SessionNext.Server | null;
@@ -16,100 +18,118 @@ export const UserCompomentInfo = ({ session }: UserCompomentInfoProps) => {
           w={"20%"}
           minH={"100%"}
           borderRight="1px solid"
-          borderColor="gray.300"
-          p={3}
+          borderColor="gray.200"
+          p={4}
           flexDir="column"
-          gap={4}
+          gap={6}
+          bg="gray.50"
+          overflowY="auto"
         >
-          <BugReport />
-          <Flex gap={4}>
-            <Flex flexDir="column" py={0} px={2} lineHeight={"1rem"}>
-              <FormLabel fontWeight={"bold"}>Id</FormLabel>
-              <Text>{session.user.id}</Text>
+          <Flex
+            bg="linear-gradient(135deg, #38A169 0%, #2F855A 100%)"
+            p={5}
+            borderRadius="xl"
+            flexDir="column"
+            gap={4}
+            color="white"
+            shadow="lg"
+          >
+            {/* Nome e Cargo */}
+            <Flex flexDir="column" gap={2} align="center">
+              <Text fontSize="xl" fontWeight="bold" textAlign="center">
+                {session.user.nome}
+              </Text>
+              <Text
+                fontSize="sm"
+                textAlign="center"
+                bg="whiteAlpha.300"
+                px={4}
+                py={1}
+                borderRadius="full"
+                fontWeight="semibold"
+              >
+                {session.user.cargo.toUpperCase()}
+              </Text>
             </Flex>
-            <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-              <FormLabel fontWeight={"bold"}>Nome</FormLabel>
-              <Text>{session.user.nome}</Text>
-            </Flex>
-            <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-              <FormLabel fontWeight={"bold"}>Cargo</FormLabel>
-              <Text>{session.user.cargo.toUpperCase()}</Text>
-            </Flex>
-          </Flex>
-          <Flex gap={4}>
-            <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-              <FormLabel fontWeight={"bold"}>Telefone</FormLabel>
-              <Text>{mask(session.user.telefone, "(99) 9 9999-9999")}</Text>
-            </Flex>
-            <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-              <FormLabel fontWeight={"bold"}>Hierarquia</FormLabel>
-              <Text>{session.user.hierarquia}</Text>
-            </Flex>
-          </Flex>
-          {session.user.construtora.length > 1 &&
-            session.user.construtora.map(
-              (item: SessionNext.Construtora, index: number) => (
-                <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-                  <FormLabel fontWeight={"bold"}>
-                    Construtora {index + 1}
-                  </FormLabel>
-                  <Text>{item.fantasia}</Text>
-                </Flex>
-              )
-            )}
-          {session.user.construtora.length === 1 &&
-            session.user.construtora.map((item: SessionNext.Construtora) => (
-              <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-                <FormLabel fontWeight={"bold"}>Construtora</FormLabel>
-                <Text>{item.fantasia}</Text>
-              </Flex>
-            ))}
-          {session.user.empreendimento.length > 1 &&
-            session.user.empreendimento.map(
-              (item: SessionNext.Empreendimento, index: number) => (
-                <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-                  <FormLabel fontWeight={"bold"}>
-                    Empreendimento {index + 1}
-                  </FormLabel>
-                  <Text>{item.nome}</Text>
-                </Flex>
-              )
-            )}
-          {session.user.empreendimento.length === 1 &&
-            session.user.empreendimento.map(
-              (item: SessionNext.Empreendimento) => (
-                <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-                  <FormLabel fontWeight={"bold"}>Empreendimento</FormLabel>
-                  <Text>{item.nome}</Text>
-                </Flex>
-              )
-            )}
-          {session.user.Financeira.length > 1 &&
-            session.user.Financeira.map(
-              (item: SessionNext.Financeira, index: number) => (
-                <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-                  <FormLabel fontWeight={"bold"}>
-                    Financeira {index + 1}
-                  </FormLabel>
-                  <Text>{item.fantasia}</Text>
-                </Flex>
-              )
-            )}
-          {session.user.Financeira.length === 1 &&
-            session.user.Financeira.map((item: SessionNext.Financeira) => (
-              <Flex flexDir="column" px={2} lineHeight={"1rem"}>
-                <FormLabel fontWeight={"bold"}>Financeira</FormLabel>
-                <Text>{item.fantasia}</Text>
-              </Flex>
-            ))}
-          <BtnListNow />
 
-          {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
+            <Flex gap={2} flexWrap="wrap" justify="center">
+              <Flex
+                flexDir="column"
+                align="center"
+                bg="whiteAlpha.200"
+                p={3}
+                borderRadius="lg"
+                flex="1"
+                minW="80px"
+              >
+                <FormLabel fontSize="xs" fontWeight="bold" color="whiteAlpha.800" mb={1}>ID</FormLabel>
+                <Text fontSize="sm" fontWeight="bold">{session.user.id}</Text>
+              </Flex>
+              <Flex
+                flexDir="column"
+                align="center"
+                bg="whiteAlpha.200"
+                p={3}
+                borderRadius="lg"
+                flex="1"
+                minW="120px"
+              >
+                <FormLabel fontSize="xs" fontWeight="bold" color="whiteAlpha.800" mb={1}>Telefone</FormLabel>
+                <Text fontSize="sm" fontWeight="bold">{mask(session.user.telefone, "(99) 9 9999-9999")}</Text>
+              </Flex>
+
+              <Flex
+                flexDir="column"
+                align="center"
+                bg="whiteAlpha.200"
+                p={3}
+                borderRadius="lg"
+                flex="1"
+                minW="100px"
+              >
+                <FormLabel fontSize="xs" fontWeight="bold" color="whiteAlpha.800" mb={1}>Hierarquia</FormLabel>
+                <Text fontSize="sm" fontWeight="bold">{session.user.hierarquia}</Text>
+              </Flex>
+            </Flex>
+          </Flex>
+
+          <BugReport />
+
+          <FinanceiraLinks userId={session.user.id} />
+
+          <Flex flexDir="column" gap={3}>
+            <Text fontSize="sm" fontWeight="bold" color="gray.600" textTransform="uppercase">
+              Construtora{session.user.construtora.length > 1 ? 's' : ''}
+            </Text>
+            <Text fontSize="sm" fontWeight="bold" textTransform="uppercase">
+              {session.user.construtora.length === 0 && 'Sem Construtoras'}
+              {session.user.construtora.length > 0 && `${session.user.construtora.length} Construtora(s)`}
+            </Text>
+          </Flex>
+
+          <Flex flexDir="column" gap={3}>
+            <Text fontSize="sm" fontWeight="bold" color="gray.600" textTransform="uppercase">
+              Empreendimento{session.user.empreendimento.length > 1 ? 's' : ''}
+            </Text>
+            <Text fontSize="sm" fontWeight="bold" textTransform="uppercase">
+              {session.user.empreendimento.length === 0 && 'Sem Empreendimentos'}
+              {session.user.empreendimento.length > 0 && `${session.user.empreendimento.length} Empreendimento(s)`}
+            </Text>
+          </Flex>
+
+          <Flex flexDir="column" gap={3}>
+            <Text fontSize="sm" fontWeight="bold" color="gray.600" textTransform="uppercase">
+              Financeira{session.user.Financeira.length > 1 ? 's' : ''}
+            </Text>
+            <Text fontSize="sm" fontWeight="bold" textTransform="uppercase">
+              {session.user.Financeira.length === 0 && 'Sem Financeiras'}
+              {session.user.Financeira.length > 0 && `${session.user.Financeira.length} Financeira(s)`}
+            </Text>
+          </Flex>
+
+          <BtnListNow />
         </Flex>
       )}
     </>
   );
-
-  //TODO:  criar lista chamados
-  //TODO:  criar lista alerts ativos
 };
