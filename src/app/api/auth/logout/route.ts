05a260bd327d8f4766f4e5e71d1a563cde66bf46
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-08-25 13:11:43 -0300

    🐛 fix: Redireciona para /login após logout
    - Utiliza NextResponse.redirect para redirecionar corretamente após a exclusão da sessão.

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,9 +1,9 @@
 import { DeleteSession } from "@/lib/auth_confg";
-import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET() {
-    await DeleteSession();
-    redirect("/login");
+export async function GET(req: NextRequest) {
+  await DeleteSession();
+  return NextResponse.redirect(new URL("/login", req.url));
 }
