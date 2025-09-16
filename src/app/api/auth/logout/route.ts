a651e37027b78f5dfef9387910c4647b08bfe1b2
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-09-16 08:41:38 -0300

    ✨ feat: Adiciona configuração do pm2 e corrige logout
    - Adiciona arquivo  para configurar o PM2
    - Corrige redirecionamento no logout para usar
    - Remove o hook  desnecessário no componente
    - Refatora a função de roteamento no componente  para melhor tratamento de erros no logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,13 +1,15 @@
-import { DeleteSession, GetSessionServer } from "@/lib/auth_confg";
-import { NextResponse } from "next/server";
+import { DeleteSession } from "@/lib/auth_confg";
+import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET() {
-  await DeleteSession();
-  const session = await GetSessionServer();
-  if (!session) {
-    return NextResponse.json({ message: "Logout successful" }, { status: 200 });
+export async function GET(req: NextRequest) {
+  try {
+    await DeleteSession();
+    return NextResponse.redirect(new URL("/login", req.url));
+  } catch (error) {
+    console.error("Erro no logout:", error);
+    // Mesmo com erro, redireciona para login
+    return NextResponse.redirect(new URL("/login", req.url));
   }
-  return NextResponse.json({ message: "Logout failed" }, { status: 401 });
 }
